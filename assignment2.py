c5042f748a81b85db93def97d823e282ff004c98
Axel Rios <98181821+ARios2022@users.noreply.github.com>
2024-01-26 10:24:16 -0800

    Update assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -20,7 +20,7 @@ class Assignment2:
         year_str = str(self.year)
         first_part = year_str[:2] * n
         second_part = ''.join([year_str[i] for i in range(len(year_str)) if i % 2 == 0]) * n
-        second_part = "78"
+        second_part = "51"
         return first_part + second_part
 
     # Task 5: Loop and Conditional statements
